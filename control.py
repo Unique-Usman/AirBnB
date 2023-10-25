26275a933751562479de8858d0539a4c87eefc46
Unique-Usman <usmanakinyemi202@gmail.com>
2023-10-25 22:32:35 +0530

    Added do_destroy method which remove an instance of an object specify and also do_all which print all string representation of all the instance of the object specify

--- control.py
+++ control.py
@@ -13,7 +13,8 @@ class HBNBCommand(cmd.Cmd):
     The entry point of the project
     """
     prompt = "(hbnb) "
-    CLASSNAME = ["BaseModel", "User"]
+    CLASSNAME = ["BaseModel", "User", "State", "City", "Place"]
+
     def do_EOF(self, line) -> bool:
         """Function to exit the interpreter gracefully"""
         return True
@@ -28,6 +29,10 @@ class HBNBCommand(cmd.Cmd):
         """
         pass
 
+    def precmd(self, line) -> str:
+        """It remove trailing and leading space and \n from the argument"""
+        return line.strip()
+
     def do_create(self, arg=None) -> None:
         """This command is for creating object
 
@@ -36,7 +41,7 @@ class HBNBCommand(cmd.Cmd):
         Args:
             arg (str): The name of the object
         """
-        if arg is None or not arg.strip():
+        if arg is None or not arg:
             print("** class name missing **")
         elif arg == "BaseModel":
             bm = BaseModel()
@@ -45,9 +50,17 @@ class HBNBCommand(cmd.Cmd):
         else:
             print("** class doesn't exist **")
 
-    def do_show(self, arg):
+    def do_show(self, arg) -> None:
+        """This command is for showing instance
+
+        The instance can be instance of any objects out of
+        BaseModel, User, State, City, Place.
+
+        To use the command :- 
+        show <classname> <instance_id>
+        """
         arg = arg.split(" ")
-        if len(arg) == 0:
+        if not arg[0]:
             print("** class name missing **")
         elif arg[0] not in HBNBCommand.CLASSNAME:
             print("** class doesn't exist **")
@@ -57,15 +70,50 @@ class HBNBCommand(cmd.Cmd):
             all_objects = storage.all()
             bm = ".".join(arg)
             if bm not in all_objects:
-                #print(bm)
                 print("** no instance found **")
             else:
-                #clss = eval(arg[0])
                 print(all_objects[bm])
+
+    def do_destroy(self, arg) -> None:
+        """This command is for destroying instance
+
+        The instance can be instance of any objects out of
+        BaseModel, User, State, City, Place.
+
+        To use the command :- 
+        destroy <classname> <instance_id>
+        """
+        arg = arg.split(" ")
+        if not arg[0]:
+            print("** class name missing **")
+        elif arg[0] not in HBNBCommand.CLASSNAME:
+            print("** class doesn't exist **")
+        elif len(arg) == 1:
+            print("** instance id missing **")
+        else:
+            all_objects = storage.all()
+            bm = ".".join(arg)
+            if bm not in all_objects:
+                print("** no instance found **")
+            else:
+                del all_objects[f"{bm}"]
+                storage.save()
+
+    def do_all(self, arg) -> None:
+        """Return all the string representation of all instance base on the classname"""
+
+        if arg not in HBNBCommand.CLASSNAME:
+            print("** class doesn't exist **")
+        else:
+            all_objects = storage.all()
+            for key in all_objects:
+                if arg in key:
+                    print(all_objects[key])
 
     # assigning the value of EOF to quit.
     do_quit = do_EOF
     help_quit = help_EOF
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
